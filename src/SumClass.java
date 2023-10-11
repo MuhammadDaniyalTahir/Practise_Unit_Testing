7e2361e1d58286f1a7e67c7b9807ff205b2789ea
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2023-10-12 00:37:29 +0500

    Initial commit

---
+++ src/SumClass.java
@@ -0,0 +1,5 @@
+public class SumClass {
+    public int sum(final int a, final int b){
+        return (a+b);
+    }
+}
---
+++ src/SumClassTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumClassTest {
+    @Test
+    void checkSum(){
+        SumClass sc = new SumClass();
+        sc.sum(1, 2);
+    }
+}
